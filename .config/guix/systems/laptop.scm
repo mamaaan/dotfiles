4e5289b6c333b07ccb55035ed18db406ead53c12
mamaaan <98651590+mamaaan@users.noreply.github.com>
2022-02-02 11:43:50 +0000

    Add files via upload
    Add the system config files for the initial install

---
+++ .config/guix/systems/laptop.scm
@@ -0,0 +1,26 @@
+(define-module (laptop)
+  #:use-module (base-system)
+  #:use-module (gnu)
+  #:use-module (nongnu packages linux))
+
+(operating-system
+ (inherit base-operating-system)
+ (host-name "laptop")
+
+ (mapped-devices
+  (list (mapped-device
+         (source (uuid "4be4fa09-0d61-4206-9099-71fde15c9b23"))
+         (target "system-root")
+         (type luks-device-mapping))))
+
+ (file-systems (cons*
+                (file-system
+                 (device (file-system-label "system-root"))
+                 (mount-point "/")
+                 (type "ext4")
+                 (dependencies mapped-devices))
+                (file-system
+                 (device "/dev/nvme0n1p1")
+                 (mount-point "/boot/efi")
+                 (type "vfat"))
+                %base-file-systems)))
