a7a4612b325c76324dacd0046686fb52d0792147
mamaaan <98651590+mamaaan@users.noreply.github.com>
2022-02-13 16:58:53 +0000

    Create desktop.scm

---
+++ .config/guix/manifests/desktop.scm
@@ -0,0 +1,32 @@
+;; Window Manager
+"stumpwm"
+"stumpwm:lib"
+"sbcl-stumpwm-swm-gaps"
+"sbcl-stumpwm-ttf-fonts"
+"sbcl"
+
+;; Settin gs Manager
+"xsettingsd"
+
+;; GTK Themes
+"arc-icon-theme"
+"matcha-theme"
+"hicolor-icon-theme"
+"gnome-icon-theme"
+"gnome-backgrounds"
+"papirus-icon-theme"
+"breeze-icons"
+
+;; Fonts
+"font-fira-code"
+"font-jetbrains-mono"
+"font-iosevka"
+"font-iosevka-aile"
+"font-abattis-cantarell"
+"font-dejavu"
+"font-google-noto"
+"font-gnu-freefont"
+"font-liberation"
+"font-awesome"
+"font-google-material-design-icons"
+"gs-fonts"
